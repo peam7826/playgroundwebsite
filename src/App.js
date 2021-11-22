8ff3ac1f3758a72874d6ad20f3a222285348dd70
peam7826 <anupatra.r@outlook.com>
2021-11-22 22:41:42 +0700

    deletebanner

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import NavigationBar from './components/Navigationbar.js';
-import Banner from './components/Banner.js';
 
 function App() {
   return (
