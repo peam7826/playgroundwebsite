e65819b414529a1a207a440fed79009057c3f2c4
Pasito16 <p.pornpimarn@gmail.com>
2021-11-22 23:19:54 +0700

    welcomebartest

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavigationBar from './components/Navigationbar.js';
+import Welcomebar from './components/Welcomebar.js';
 
 function App() {
   return (
     <div className="App">
         <NavigationBar />
+        <Welcomebar />
     </div>
   );
 }
---
+++ src/components/Welcomebar.js
@@ -0,0 +1,11 @@
+import './Welcomebar.css';
+
+function Welcomebar(){
+    return(
+    <div className = "Welcomebar">
+    <h2>WELCOME</h2>  
+    <h2>KUY TOY</h2>
+    </div>
+    )
+}
+export default Welcomebar
