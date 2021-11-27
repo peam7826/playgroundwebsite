aa8309ec3c9e9639a28e90630483cad9f4af239f
Pasito16 <p.pornpimarn@gmail.com>
2021-11-27 16:35:31 +0700

    welcomebar27/11

--- src/components/Welcomebar.js
+++ src/components/Welcomebar.js
@@ -3,7 +3,7 @@ import "./Welcomebar.css";
 function Welcomebar() {
   return (
     <div className="Welcomebar">
-      <p>WELCOME! Anupatra Ruchutrakool</p>
+      <p>WELCOME! <br />Anupatra Ruchutrakool</p>
     </div>
   );
 }
