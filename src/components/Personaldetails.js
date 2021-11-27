edbc1eeea77613a9e281e006dd01d457ef4bca27
Pasito16 <p.pornpimarn@gmail.com>
2021-11-27 18:45:25 +0700

    Personal-details

---
+++ src/components/Personaldetails.js
@@ -0,0 +1,15 @@
+import "./Personaldetails.css";
+
+function Personaldetails() {
+  return (
+    <div className="Personaldetails">
+      <p>Personal Details</p>
+      <div className="Personaldetails2">
+        <p>Fullname <br /> Anupat Rujutracool 
+        <br /><br /><br />Address <br />337 Soi 6 Pattannakarn53 Pattanakarn Road Suanluang Bangkok 10250
+        <br /><br /><br />Email <br />anupatzakung007@hotmail.com</p>
+      </div>
+    </div>
+  );
+}
+export default Personaldetails;
